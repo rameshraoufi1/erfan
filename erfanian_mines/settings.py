48da796649566f5fe579c629846894cc86278e0e
rameshraoufi1 <ramesh.raoufi@lajward.dev>
2025-04-23 23:42:12 +0430

    correct style

--- erfanian_mines/settings.py
+++ erfanian_mines/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&md!wusxdolba=*-aakpfhia(ha+tevla457a$)d5#i0xzx6+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -134,7 +134,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
